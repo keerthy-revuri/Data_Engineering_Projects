45b6e9be72be5f35999fe1f436e19991a7826ef1
keerthy-revuri <keerthyreddy9991@gmail.com>
2024-06-26 14:06:34 -0400

    adding update github project files

--- Github_data_to_excel/load_script.sql
+++ Github_data_to_excel/load_script.sql
@@ -12,16 +12,20 @@ CSV HEADER;
 
 DELETE FROM postgres.public.github_staging where (category, file_names) is null;
 
+
 DELETE FROM postgres.public.github_target using postgres.public.github_staging where github_staging.category = github_target.category
 and github_staging.file_names = github_target.file_names;
 
-drop table if exists temp_github_staging;
+DROP table if temp_github_staging exists;
 
-create temporary table temp_github_staging
-as SELECT category,
-          file_names,
-          updated_dtm
-FROM postgres.public.github_staging;
+CREATE temporary table temp_github_staging as
+SELECT Category, file_names, max(updated_dtm) from
+postgres.public.github_staging group by category, file_names;
+
+DELETE FROM postgres.public.github.staging using temp_github_staging
+where github_staging.file_names = temp_github_staging.file_names and
+github_staging.category = temp_github_staging.category
+and github_staging.updated_dtm <> temp_github_staging.updated_dtm;
 
 INSERT INTO postgres.public.github_target(
     category,
@@ -32,8 +36,5 @@ SELECT category,
        file_names,
        updated_dtm
 FROM temp_github_staging
-group by
-category,
-file_names,
- updated_dtm
+
 
